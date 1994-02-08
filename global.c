48f530a5c4cec893dbbb803b762559b0b23b6778
spb <spb>
1994-02-08 10:02:30 +0000

    changed defaults a bit.

--- global.c
+++ global.c
@@ -2,27 +2,27 @@
 #include "paint.h"
 
 Fold *top;
-int levels = 9;
-int stop = 1;
+int levels = 10;
+int stop = 2;
 int smooth = TRUE;
 int frac_start = TRUE;
 int slope = TRUE;
 float fdim = 0.65;
-float start;      /* starting value for the surface */
-float mean=0.0;   /* mean value of surface */
-float varience;   /* rough estimate of the height of the range */
-float shift=0.5;      /* offset from calcalt to artist coordinates */
+Height start;      /* starting value for the surface */
+Height mean=0.0;   /* mean value of surface */
+Height varience;   /* rough estimate of the height of the range */
+Height shift=0.5;    /* offset from calcalt to artist coordinates */
 float stretch=0.6;   /* vertical stretch */
 float contour = 0.3;
-float ambient = 0.49;  /* level of ambient light */
+float ambient = 0.3;  /* level of ambient light */
 float contrast = 1.0; /* contrast,
                        * increases or decreases effect of cosine rule */
-float vfract   = 0.4; /* relative strength of vertical light relative
+float vfract   = 0.6; /* relative strength of vertical light relative
                         * to the main light source
                         */
 float altitude = 2.5;
 float distance = 4.0;
-double phi=(45.0 * PI)/180.0; /* angle of the light */
+double phi=(40.0 * PI)/180.0; /* angle of the light */
 double cos_phi;
 double sin_phi;
 double tan_phi;
