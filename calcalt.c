975ad292285a367c69c791d8bd85dd25ecff5ebe
spb <spb>
1994-07-01 09:34:52 +0000

    version 2 wither better regen and cross updates

--- calcalt.c
+++ calcalt.c
@@ -23,7 +23,7 @@
 #include <math.h>
 #include "crinkle.h"
 
-char calcalt_Id[] = "$Id: calcalt.c,v 1.9 1994/02/02 18:48:41 spb Exp $";
+char calcalt_Id[] = "$Id: calcalt.c,v 2.0 1994/07/01 09:34:52 spb Exp $";
 
 /*{{{  Strip *make_strip(int level) */
 Strip *make_strip (level)
@@ -103,207 +103,36 @@ Height value;
   return(s);
 }
 /*}}}*/
-/*{{{  void side_update(Strip *strip, Length scale) */
-/* fill in the blanks in a strip that has just been doubled
- * this could be combined with the double routine but it would
- * make the code even messier than it already is
- */
-void side_update (strip,scale)
-Strip *strip;
-Length scale;
-{
-  int count;
-  int i;
-  Height *p;
-
-  count = ( 1 << (strip->level - 1));
-  p = strip->d;
-  for(i=0 ; i<count ; i++)
-  {
-    *(p+1) = ((scale * gaussian()) + (*p + *(p+2))/2.0) ;
-    p += 2;
-  }
-}
-/*}}}*/
-/*{{{  void mid_update(Strip *left, Strip *new, Strip *right,Length scale, Length midscale) */
-/* Calculate a new strip using the two strips to either side.
- * the "left" strip should be only half the size of the other
- * two.
- */
-void mid_update (left,new,right,scale,midscale)
-Strip *left;
-Strip *new;
-Strip *right;
-Length scale;
-Length midscale;
-{
-  int count;
-  int i;
-  Height *l , *n , *r;
-
-  if( (left->level != (new->level - 1)) || (new->level != right->level))
-  {
-    fprintf(stderr,"mid_update: inconsistant sizes\n");
-    exit(2);
-  }
-  count = ( 1 << left->level);
-  l = left->d;
-  n = new->d;
-  r = right->d;
-  for(i=0 ; i<count ; i++)
-  {
-    *n = ((scale * gaussian()) + (*l + *r)/2.0) ;
-    n++;
-    *n = ((midscale * gaussian()) + (*l + *(l+1) + *r + *(r+2))/4.0) ;
-    n++;
-    l++;
-    r += 2;
-  }
-  /* the last one */
-  *n = ((scale * gaussian()) + (*l + *r)/2.0) ;
-}
-/*}}}*/
-/*{{{  void recalc(Strip *left, Strip *regen, Strip *right,Length scale) */
-/* Recalculate all the old values using the points we have just 
- * generated. This is a little idea of mine to get rid of the
- * creases. However it may change the effective fractal dimension
- * a litle bit. But who cares ?
- */
-void recalc (left,regen,right,scale)
-Strip *left;
-Strip *regen;
-Strip *right;
-Length scale;
-{
-  int count;
-  int i;
-  Height *l , *g , *r;
-
-  if((left->level != regen->level) || (regen->level != right->level))
-  {
-    fprintf(stderr,"recalc: inconsistant sizes\n");
-    exit(2);
-  }
-  count = ( (1 << (regen->level-1)) - 1 );
-  l = left->d;
-  g = regen->d;
-  r = right->d;
-  *g = ((scale * gaussian()) + (*l + *(g+1) + *r)/3.0) ;
-  g += 2;
-  l += 2;
-  r += 2;
-  for(i=0 ; i<count ; i++)
-  {
-    *g = ((scale * gaussian()) + (*l + *(g+1) + *(g-1) + *r)/4.0) ;
-    g += 2;
-    l += 2;
-    r += 2;
-  }
-  /* the last one */
-  *g = ((scale * gaussian()) + (*l + *(g-1) + *r)/3.0) ;
-}
-/*}}}*/
-/*{{{  Strip *next_strip(Fold *fold) */
-Strip *next_strip (fold)
-Fold *fold;
-{
-  Strip *result;
-  int i;
-
-  if( fold->level == fold->stop)
-  {
-    /*{{{  generate values from scratch */
-    result=make_strip(fold->stop);
-    i=0;
-    if( fold->slope )
-    {
-      result->d[i] = fold->mean;
-      i++;
-    }
-    for( ; i < ((1 << fold->stop) +1) ; i++)
-    {
-      result->d[i] = fold->mean + (fold->scale * gaussian());
-    }
-    return(result);
-    /*}}}*/
-  }
-  switch(fold->state)
-  {
-    case START:
-      /*{{{  perform an update. return first result */
-      /*
-       * new is NULL
-       * working is NULL
-       * regen is a partial strip, only odd values are valid,
-       * old is a fully calculated strip
-       */
-      fold->new = next_strip(fold->next);
-      side_update(fold->regen,fold->scale);
-      fold->working = make_strip(fold->level);
-      mid_update(fold->new,fold->working,fold->regen,
-                  fold->scale,fold->midscale);
-      if( fold->smooth )
-      {
-        recalc(fold->working,fold->regen,fold->old,fold->scale);
-      }
-      result = fold->old;
-      fold->old = NULL;
-      fold->state = STORE;
-      return(result);
-      /*}}}*/
-    case STORE:
-      /*{{{  return second value from previous update. */
-      result = fold->regen;
-      fold->old = fold->working;
-      fold->working = NULL;
-      fold->regen = double_strip(fold->new);
-      free_strip(fold->new);
-      fold->new = NULL;
-      fold->state = START;
-      return(result);
-      /*}}}*/
-    default:
-      fprintf(stderr,"next_strip: invalid state\n");
-      exit(3);
-  }
-  return(NULL);
-}
-/*}}}*/
 /*{{{  Fold *make_fold(int levels, int stop, int fractal_start, int slope,int smooth, Length len, Height start, Height mean, float fdim) */
 /*
  * Initialise the fold structures.
  * As everything else reads the parameters from their fold
  * structs we need to set these here,
+ * p  is the parameter struct common to all update levels.
  * levels is the number of levels of recursion below this one.
  *    Number of points = 2^levels+1
  * stop is the number of levels that are generated as random offsets from a
  *    constant rather than from an average.
  * fractal_start, if true we start in the middle of a mountain range
- *    if false we build up mountains from the start.
- * slope, force the front of the fractal to remain close to the mean value
- * smooth turns the smoothing algorithm on or off
- * len is the length of the side of the square at this level.
+ *    if false we build up mountains from the start height.
+ * length is the length of the side of the square at this level.
  *   N.B this means the update square NOT the width of the fractal.
  *   len gets smaller as the level increases.
- * start, see uder fractal_start.
- * mean is the mean height.
- * fdim is the fractal dimension
+ * start, the starting height for a non-fractal start.
  */
-Fold *make_fold (levels,stop,fractal_start,slope,smooth,length,start,mean,fdim)
+Fold *make_fold (param,levels,stop,fractal_start,length,start)
+struct parm *param;
 int levels;
 int stop;
 int fractal_start;
-int slope;
-int smooth;
 Length length;
 Height start;
-Height mean;
-float fdim;
 {
   Fold *p;
   Length scale, midscale;
   double root2;
   Strip *tmp;
+  int i;
 
   if( (levels < stop) || (stop<0) )
   {
@@ -318,47 +147,33 @@ float fdim;
     exit(1);
   }
   root2=sqrt((double) 2.0 );
-  scale = pow((double) length, (double) (2.0 * fdim));
-  midscale = pow((((double) length)*root2), (double) (2.0 * fdim));
+  scale = pow((double) length, (double) (2.0 * param->fdim));
+  midscale = pow((((double) length)*root2), (double) (2.0 * param->fdim));
   p->level = levels;
   p->stop = stop;
   p->state = START;
-  p->smooth = smooth;
-  p->slope = slope;
-  p->mean = mean;
+  p->p = param;
   p->scale = scale;
   p->midscale = midscale;
-  p->new = NULL;
-  p->working = NULL;
+  for(i=0;i<NSTRIP;i++)
+  p->s[i] = NULL;
   if( levels > stop )
   {
-    p->next = make_fold((levels-1),stop,fractal_start,slope,smooth,(2.0*length),start,mean,fdim);
+    p->next = make_fold(param,(levels-1),stop,fractal_start,(2.0*length),start);
     if ( fractal_start )
     {
-      tmp = next_strip( p->next );
-      p->regen = double_strip(tmp);
-      free_strip(tmp);
-      p->old = NULL;
       /*
-       * do an update step to flush out the undefined p->old
-       * pointer, as p->old is used in the regeneration step we
-       * have to force smoothing to be off. This should return a NULL
-       * pointer
+       * flush out the NULL pointers so the
+       * pipeline is ready to run
        */
-      p->smooth = FALSE;
-      if( NULL != next_strip( p ) )
+      while( p->s[0] == NULL )
       {
-        fprintf(stderr,"make_fold: internal logic error\n");
-        exit(2);
+        next_strip(p);
       }
-      p->smooth = smooth;
     }else{
-      p->regen = set_strip(levels,start);
-      p->old = set_strip(levels,start);
+      p->s[NSTRIP-2] = set_strip(p->level,start);
     }
   }else{
-    p->regen = NULL;
-    p->old = NULL;
     p->next = NULL;
   }
   return( p );
@@ -368,32 +183,107 @@ float fdim;
 void free_fold (f)
 Fold *f;
 {
-  if( f->new != NULL )
+  int i;
+  for(i=0;i<NSTRIP;i++)
   {
-    free_strip(f->new);
-    f->new = NULL;
-  }
-  if( f->working != NULL )
-  {
-    free_strip(f->working);
-    f->working = NULL;
-  }
-  if( f->regen != NULL )
-  {
-    free_strip(f->regen);
-    f->regen = NULL;
-  }
-  if( f->old != NULL )
-  {
-    free_strip(f->old);
-    f->old = NULL;
-  }
-  if( f->next != NULL )
-  {
-    free_fold(f->next);
-    f->next = NULL;
+    if( f->s[i] != NULL )
+    {
+      free_strip(f->s[i]);
+      f->s[i] = NULL;
+    }
   }
   free(f);
   return;
 }
 /*}}}*/
+
+/*{{{  Strip *next_strip(Fold *fold) */
+Strip *next_strip (fold)
+Fold *fold;
+{
+  Strip *result, *tmp;
+  int i, count;
+
+  count = ((1 << fold->stop) +1);
+  if( fold->level == fold->stop)
+  {
+    /*{{{  generate values from scratch */
+    result=make_strip(fold->stop);
+    for( i=0 ; i < count ; i++)
+    {
+      result->d[i] = fold->p->mean + (fold->scale * gaussian());
+    }
+    if( fold->p->force_front ){
+      result->d[0] = fold->p->mean;
+    }
+    if( fold->p->force_back ){
+      result->d[count-1] = fold->p->mean;
+    }
+    return(result);
+    /*}}}*/
+  }
+  switch(fold->state)
+  {
+    case START:
+      /*{{{  perform an update. return first result*/
+      tmp=NULL;
+      while( tmp == NULL )
+      {
+        tmp = next_strip(fold->next);
+      }
+      fold->s[7] = double_strip(tmp);
+      x_update(count,fold->midscale,0.0,fold->s[5],fold->s[6],fold->s[7]);
+      if(fold->p->rg1)
+      {
+        v_update(count,fold->midscale,fold->p->mix,fold->s[4],fold->s[5],fold->s[6]);
+      }
+      if( fold->p->cross )
+      {
+        t_update(count,fold->scale,0.0,fold->s[3],fold->s[4],fold->s[5]);
+        p_update(count,fold->scale,0.0,fold->s[2],fold->s[3],fold->s[4]);
+      }else{
+        vside_update(count,fold->scale,0.0,fold->s[4]);
+        hside_update(count,fold->scale,0.0,fold->s[2],fold->s[3],fold->s[4]);
+      }
+      if(fold->p->rg2)
+      {
+        if( fold->p->cross )
+        {
+          p_update(count,fold->scale,fold->p->mix,fold->s[1],fold->s[2],fold->s[3]);
+        }else{
+          vside_update(count,fold->scale,fold->p->mix,fold->s[2]);
+        }
+      }
+      if(fold->p->rg3)
+      {
+        if( fold->p->cross )
+        {
+          t_update(count,fold->scale,fold->p->mix,fold->s[0],fold->s[1],fold->s[2]);
+        }else{
+          hside_update(count,fold->scale,fold->p->mix,fold->s[0],fold->s[1],fold->s[2]);
+        }
+      }
+      
+      result=fold->s[0];
+      fold->state = STORE;
+      return(result);
+      /*}}}*/
+    case STORE:
+      /*{{{  return second value from previous update. */
+      result = fold->s[1];
+      for(i=0;i<(NSTRIP-2);i++)
+      {
+        fold->s[i] =fold->s[i+2];
+      }
+      fold->s[NSTRIP-2] = fold->s[NSTRIP-1]=NULL;
+      fold->state = START;
+      return(result);
+      /*}}}*/
+    default:
+      fprintf(stderr,"next_strip: invalid state\n");
+      exit(3);
+  }
+  return(NULL);
+}
+/*}}}*/
+
